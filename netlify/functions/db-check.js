ebf3dbe74f57bf1b417ce9ae11b2f11338c69ae7
Armando José Ovalle <ovalle_938@hotmail.com>
2025-09-23 21:30:05 -0500

    Update db-check.js

--- netlify/functions/db-check.js
+++ netlify/functions/db-check.js
@@ -1,12 +1,10 @@
 export async function handler() {
-  const hasDbUrl = !!process.env.DATABASE_URL;
-
   return {
     statusCode: 200,
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
       ok: true,
-      has_DATABASE_URL: hasDbUrl
+      has_DATABASE_URL: !!process.env.DATABASE_URL
     })
   };
 }
